bac1b0768ebde70b79021ce80b40ae98e01499bf
Alexander Mamaev <alxmamaev@ya.ru>
2017-12-25 09:56:52 +0500

    Add apertium parser

--- converter.py
+++ converter.py
@@ -1,4 +1,5 @@
 import sys
+import re
 import codecs
 
 ud2apr = None
@@ -23,19 +24,53 @@ def parse_ud(input_file):
 
 		_, word, normal_form, pos, _, tags, _, _, _, _ = line.strip().split("\t")
 		
-		token = {"word":word, "normal_form": normal_form, "pos": pos, "tags": tags.split("|")}
+		token = {"word":word, "normal_form": normal_form, "pos": pos, "tags": set([ud2apr[t] for t in tags.split("|") + [pos]])}
 		ud_tree.append(token)
 
 	return (ud_tree)
 
-def convert_token_to_apertium(token):
-	result_token = "^%s/%s" % (token["word"], token["normal_form"])
+def get_apertium_tags(input_file):
+	result_tags = {}
+	while True:
+		line = input_file.readline()
+		if not line: break
+		if not line.startswith("# text = "): continue
 
-	result_token += ud2apr[token["pos"]]
+		line = line.replace("# text = ", "")
+		words = line.replace("$ ", "$").replace("^", "").strip().split("$")[:-1]
+		# import pdb;pdb.set_trace()
+		for word in words:
+			tokens = word.split("/")
+			
+			if tokens[0] in result_tags:
+				continue
 
-	for tag in token["tags"]:
-		result_token += ud2apr[tag]
-	
+			variants_of_words = []
+			for token in tokens[1:]:
+				normal_form = token.split("<")[0]
+				
+				tags = re.findall("\<[a-z]*\>", token)
+			
+				variants_of_words.append({"normal_form":normal_form, "tags":tags})
+			
+			result_tags[tokens[0]] = variants_of_words
+
+	return result_tags
+
+def convert_token_to_apertium(token, apertium_parse_result):
+	result_token = "^%s/" % (token["word"])
+
+	max_intersection = 0
+	result_tags = []
+	for variant in apertium_parse_result[token["word"]]:
+		if variant["normal_form"] != token["normal_form"]:
+			continue
+
+		if len(set(variant["tags"]) & token["tags"]) > max_intersection:
+			max_intersection = len(set(variant["tags"]) & token["tags"])
+			result_tags = variant["tags"]
+
+	result_token += "".join(result_tags)
 	result_token += "$"
 
 	result_token = result_token.replace("<>", "")
@@ -43,17 +78,20 @@ def convert_token_to_apertium(token):
 
 if __name__ == "__main__":
 	tags_file = codecs.open(sys.argv[1], "r", "utf-8")
-	input_file = codecs.open(sys.argv[2], "r", "utf-8")
-	output_file = codecs.open(sys.argv[3], "w", "utf-8")
+	apertium_file = codecs.open(sys.argv[2], "r", "utf-8")
+	input_file = codecs.open(sys.argv[3], "r", "utf-8")
+	output_file = codecs.open(sys.argv[4], "w", "utf-8")
 
 	ud2apr = get_tags(tags_file)
 	ud_tree = parse_ud(input_file)
+	apertium_parse_result = get_apertium_tags(apertium_file)
 
 	for token in ud_tree:
-		ap_token = convert_token_to_apertium(token)
+		ap_token = convert_token_to_apertium(token, apertium_parse_result)
 		print(ap_token)
 		output_file.write(ap_token+"\n")
 
+	apertium_file.close()
 	tags_file.close()
 	input_file.close()
 	output_file.close()
